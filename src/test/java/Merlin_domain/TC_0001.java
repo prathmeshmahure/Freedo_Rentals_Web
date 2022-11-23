88bd5e5f91505f18c3c56bc4e077824a006c9850
prathmeshmahure <mahureprathmesh61@gmail.com>
2022-11-23 23:55:45 +0530

    4

--- src/test/java/Merlin_domain/TC_0001.java
+++ src/test/java/Merlin_domain/TC_0001.java
@@ -13,12 +13,16 @@ public class TC_0001 extends base_class{
 	@Test
 	public void Login_FB()  {
 		login_page login = PageFactory.initElements(driver, login_page.class);
+//		login.getEmail().sendKeys("7385820122");
+		
 		
 		library.custom_sendkeys(login.getEmail(), config.getusername(), "Email");
 		library.custom_sendkeys(login.getPass(), config.getpassword(), "Password");
 		library.Custom_click(login.getButton(), "Login_Button");
-		Assert.assertTrue(false);
+		Assert.assertTrue(true);
 		
+//		login.getPass().sendKeys("Mahure@123");
+//		login.getButton().click();
 		
 	}
 
